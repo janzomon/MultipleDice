fca9d79221fc073a01019468a12c31dff7d44361
Damon Gonzalez <damon.r.gonzalez@gmail.com>
2024-11-21 13:59:01 -0800

    Initial Commit

---
+++ MultipleDice/MultipleDiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultipleDiceApp.swift
+//  MultipleDice
+//
+//  Created by Damon gonzalez on 11/21/24.
+//
+
+import SwiftUI
+
+@main
+struct MultipleDiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MultipleDiceTests/MultipleDiceTests.swift
@@ -0,0 +1,17 @@
+//
+//  MultipleDiceTests.swift
+//  MultipleDiceTests
+//
+//  Created by Damon gonzalez on 11/21/24.
+//
+
+import Testing
+@testable import MultipleDice
+
+struct MultipleDiceTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
